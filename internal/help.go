b756954ad4322022a9d75a9158a9516ea1179ffd
tshackerz71 <rjpramsagar89@gmail.com>
2025-06-14 10:43:21 +0530

    Create help.go

---
+++ internal/help.go
@@ -0,0 +1,17 @@
+package internal
+
+import "fmt"
+
+func ShowHelp() {
+    fmt.Println("📖 HELP MENU")
+    fmt.Println("[1] Host Scanner")
+    fmt.Println("[2] Subfinder")
+    fmt.Println("[3] IP Lookup")
+    fmt.Println("[4] File Toolkit")
+    fmt.Println("[5] Port Scanner")
+    fmt.Println("[6] DNS Record Lookup")
+    fmt.Println("[7] Host Info")
+    fmt.Println("[8] Help")
+    fmt.Println("[9] Update")
+    fmt.Println("[0] Exit")
+}
